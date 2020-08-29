903e5d936c9b2cf39237d3e0102146f136e8f48d
sirlocust <jua56yu@hotmail.com>
2020-08-29 01:42:15 -0500

    agregando nuevo reducer llamado filter

---
+++ src/app/filter/filter.reducer.ts
@@ -0,0 +1,16 @@
+import * as fromFiltro from './filter.actions';
+
+const estdoInicial: fromFiltro.filtrosValidos = 'todos';
+
+export function filtroReducer(
+  state = estdoInicial,
+  action: fromFiltro.acciones
+): fromFiltro.filtrosValidos {
+
+    switch(action.type){
+        case fromFiltro.SET_FILTRO:
+            return action.filtro
+        default:
+            return state
+    }
+}
